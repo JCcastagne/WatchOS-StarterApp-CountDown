39c406586188279b69ac9b82fd701cc888199b07
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2022-02-22 15:32:16 -0500

    Add core functionalities; timer countdown, controls

--- CountDown/CountDown WatchKit Extension/ContentView.swift
+++ CountDown/CountDown WatchKit Extension/ContentView.swift
@@ -8,10 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var countDownTimer = 0
+    @State var timerRunning = false
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack(){
+            Text("\(countDownTimer)")
+                .onReceive(timer) { _ in
+                    if countDownTimer > 0 && timerRunning {
+                        countDownTimer -= 1
+                    } else {
+                        timerRunning = false
+                    }
+                }
+            
+            HStack(){
+                Button {
+                    print("Increment")
+                    countDownTimer = countDownTimer + 1
+                } label: {
+                    Image(systemName: "plus")
+                }
+                Button {
+                    print("Decrement")
+                    countDownTimer = countDownTimer - 1
+                } label: {
+                    Image(systemName: "minus")
+                }
+                Button {
+                    print("Start CountDown")
+                    timerRunning = true
+                } label: {
+                    Text("Start")
+                }
+                Button {
+                    print("Stop CountDown")
+                    timerRunning = false
+                    countDownTimer = 0
+                } label: {
+                    Text("Stop")
+                }
+            }
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
